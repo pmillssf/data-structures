ea2459fa701d199a30e0f81670707bd1d22dedcf
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-08 15:18:52 -0800

    Pass 1st test

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
+  var someInstance = {'length': 0};
+  $.extend(someInstance, stackMethods);
+  console.log(someInstance);
+  return someInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  'size': function() {
+    return this.length;
+  }
+};
 
 
