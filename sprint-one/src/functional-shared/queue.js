aad042ea8126e369f3c854284aa2b095de7c348f
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-08 15:40:53 -0800

    Pass 1st test

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,15 @@
 var Queue = function() {
+  var someInstance = {'len': 0};
+  $.extend(someInstance, queueMethods);
+  return someInstance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.len;
+  }
+};
 
 
