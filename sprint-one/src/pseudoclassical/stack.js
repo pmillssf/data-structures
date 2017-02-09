a260aef5db4e2fea682b507e81392f0fcebe2ece
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-08 16:31:43 -0800

    Pass 1st test pseudoclassical pattern stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,11 @@
 var Stack = function() {
+  this.len = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-
+Stack.prototype = {
+  'size': function() {
+    return this.len;
+  }
+};
