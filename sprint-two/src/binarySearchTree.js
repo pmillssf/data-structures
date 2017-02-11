c6b8031027c0522c9d3db8168aeca143c287a1f5
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-11 09:38:12 -0800

    Pass 1st test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,14 +1,28 @@
 var BinarySearchTree = function(value) {
-// set var tree = Object.create(BinarySearchTree.prototoye)
-// set tree.value equal to value
-// set tree.left
-// set tree.right
-// return tree
+  // set var tree = Object.create(BinarySearchTree.prototype)
+  var tree = Object.create(BinarySearchTree.prototype);
+  // set tree.value equal to value
+  tree.value = value;
+  // set tree.left
+  tree.left;
+  // set tree.right
+  tree.right;
+  // return tree
+  return tree;
 };
 
-// define BinarySeatchTree.prototype.insert function
-// define BinarySeatchTree.prototype.contains funciton
-// define BinarySeatchTree.prototype.depthFirstLog function
+// define BinarySearchTree.prototype.insert function
+BinarySearchTree.prototype.insert = function() {
+
+};
+// define BinarySearchTree.prototype.contains function
+BinarySearchTree.prototype.contains = function() {
+
+};
+// define BinarySearchTree.prototype.depthFirstLog function
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
