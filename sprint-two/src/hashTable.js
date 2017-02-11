377057de44a01b97d9be11c3eeaf730c036eab46
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-10 19:53:08 -0800

    Pass 2nd-4th tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,11 +8,20 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // set pair equal to an array of [k, v]
+  var pair = [k, v];
    // set index to pair on storage
+  this._storage.set(index, pair);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // set retrieved equal to .get(index);
+  var retrieved = this._storage.get(index);
+  // if retrieved[0] === k
+  if (retrieved[0] === k) { 
+    // return retrieved[1];
+    return retrieved[1]; 
+  }
 };
 
 HashTable.prototype.remove = function(k) {
