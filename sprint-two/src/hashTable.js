7628fff27ab02ded1a119651fe016f230637afff
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-10 20:52:08 -0800

    Pass test 6, fail test 5

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,20 +9,32 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // set pair equal to an array of [k, v]
   var pair = [k, v];
-   // set index to pair on storage
-  this._storage.set(index, pair);
-// Update to handle collisions:
-// check if index is undefined
-//     if so set(index) = [];
-// set var retrieved = .retrieve(index)
-// set var keyexists = false
-// iterate through retrieved
-//   if retrieved[i][0] equals k
-//     set retieved[i][1] to v 
-//     set keyexists = true
-// if !keyexisits 
-//   push pair into retreved;
-// set index to rerieved
+  // check if .get(index) is undefined
+  if (this._storage.get(index) === undefined) {
+    //if so set(index) = [];
+    this._storage.set(index, []);
+  }
+  // set var retrieved = .get(index)
+  var retrieved = this._storage.get(index);
+  // set var keyexists = false
+  var keyExists = false;
+  // iterate through retrieved
+  for (var i = 0; i < retrieved.length; i++) {
+    //if retrieved[i][0] equals k
+    if (retrieved[i][0] === k) {
+      //set retieved[i][1] to v 
+      retrieved[i][1] = v;
+      //set keyexists = true
+      keyExists = true;  
+    }
+  }
+  // if !keyexisits 
+  if (!keyExists) {
+    //push pair into retreved;
+    retrieved.push(pair);
+  }
+  // set index to rerieved
+  this._storage.set(index, retrieved);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -30,10 +42,11 @@ HashTable.prototype.retrieve = function(k) {
   // set retrieved equal to .get(index);
   var retrieved = this._storage.get(index);
   if (retrieved !== undefined) {
-    // if retrieved[0] === k
-    if (retrieved[0] === k) { 
-      // return retrieved[1];
-      return retrieved[1]; 
+    for (var i = 0; i < retrieved.length; i++) {
+      if (retrieved[i][0] === k) { 
+        // return retrieved[1];
+        return retrieved[i][1]; 
+      }
     }
   }
 };
