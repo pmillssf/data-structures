6fe014873c9b7b6a579f7e270364259b628f7b9c
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-11 09:33:03 -0800

    Write pseudocode for 1st test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,14 @@
 var BinarySearchTree = function(value) {
+// set var tree = Object.create(BinarySearchTree.prototoye)
+// set tree.value equal to value
+// set tree.left
+// set tree.right
+// return tree
 };
 
+// define BinarySeatchTree.prototype.insert function
+// define BinarySeatchTree.prototype.contains funciton
+// define BinarySeatchTree.prototype.depthFirstLog function
 
 /*
  * Complexity: What is the time complexity of the above functions?
