a4b771e6e47ae2bedbbc98d509aedcee1094f9e9
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-10 23:09:02 -0800

    Pass all set tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -8,17 +8,25 @@ var setPrototype = {};
 
 setPrototype.add = function(item) {
   // Check if the Set contains the item
+  if (!this.contains(item)) {
     // if not this._storage.push(item)
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
   // return _.contains(this._storage, item);
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
   // set removeIndex equal to indexOf this._storage, item
+  var removeIndex = _.indexOf(this._storage, item);
   // if removeIndex > 0
+  if (removeIndex >= 0) {
     // remove the item
+    this._storage.splice(removeIndex, 1);
+  }
 };
 
 /*
