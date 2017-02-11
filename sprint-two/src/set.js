27eb0efe472a603ea79dd066ca5fd34903b7a327
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-10 23:04:36 -0800

    Write pseudocode for 2nd and 3rd tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // Check if the Set contains the item
+    // if not this._storage.push(item)
 };
 
 setPrototype.contains = function(item) {
+  // return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  // set removeIndex equal to indexOf this._storage, item
+  // if removeIndex > 0
+    // remove the item
 };
 
 /*
