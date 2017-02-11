95e22328cbe4b15bbaae9c50297183e8638c0c93
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-11 10:17:14 -0800

    Write pseudocode for 2nd test, pass minimum insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -14,10 +14,33 @@ var BinarySearchTree = function(value) {
 // define BinarySearchTree.prototype.insert function
 BinarySearchTree.prototype.insert = function(value) {
   // set var newBinarySearchTree = BinarySearchTree(value);
+  var newBinarySearchTree = BinarySearchTree(value);
   // if this.value > newBinarySearchTree.value
+  if (this.value > newBinarySearchTree.value) {
     // set this.left to newBinarySearchTree
+    this.left = newBinarySearchTree;
   // else if this.value < newBinarySearchTree
+  } else if (this.value < newBinarySearchTree.value) {
     // set this.right to newBinarySearchTree
+    this.right = newBinarySearchTree;
+  }
+  // reset parameter to treeOrValue
+  // set var instantiating = false;
+  // if typeof treeOrValue === number
+    // var newBinarySearchTree = BinarySearchTree(treeOrValue);
+    // instantiating = true
+  // if (instiating) 
+    //  this.insert(newBinarySearchTree)
+  // if (this.value > treeOrValue.value)
+    // if this.left === undefined
+      // this.left = treeOrValue
+    // else 
+      // this.left.insert(treeOrValue);
+  // if (this.value < treeOrValue.value)
+    // if this.right === undefined
+      // this.right = treeOrValue
+    // else 
+      //this.right.inset(treeOrValue) 
 
 };
 // define BinarySearchTree.prototype.contains function
